768e486d8fa5ab10cbc624b845df461fc23403dc
sepidehparhami <sepidehparhami@gmail.com>
2018-06-14 17:00:20 -0700

    continue affy expression

--- code/01_makeDatExpr.R
+++ code/01_makeDatExpr.R
@@ -2,16 +2,17 @@
 
 options(stringsAsFactors = FALSE)
 #source("http://bioconductor.org/biocLite.R")
-#biocLite("GEOquery")
+#biocLite("oligo")
 
 setwd("/u/project/gandalm/sparhami/PsychotropicTx")
 
 library(affy)
 library(GEOquery)
+library(oligo)
 
 
-# function to extract metadata and counts for raw Affymetrix sets
-ExtractAffy <- function(accession,experiment){
+# function to extract metadata and expression for raw Affymetrix sets
+ExtractAffy <- function(accession,experiment,oligo=NULL){
   gse <- getGEO(accession, GSEMatrix=TRUE,getGPL=FALSE)
   datMeta <- pData(gse[[1]])
   rownames(datMeta) <- datMeta[,2]
@@ -21,26 +22,30 @@ ExtractAffy <- function(accession,experiment){
   celpath <- paste0("raw_data/",experiment)
   listdir <- list.files(celpath)
   celfiles <- listdir[grep("CEL",listdir,ignore.case=TRUE)]
-  data.affy <- ReadAffy(celfile.path = celpath, filenames = celfiles)
+  
+  if (oligo){
+    data.affy <- read.celfiles(paste0(celpath,"/",celfiles))
+  }else{
+    data.affy <- ReadAffy(celfile.path = celpath, filenames = celfiles)
+  }
+  
   datExpr <- exprs(data.affy)
-  
-  
-  filemeta <- paste0("datMeta_",accession,".RData")
-  save(datMeta,file = paste0(celpath,"/",filemeta))
-  fileexpr <- paste0("datExpr_",accession,".RData")
-  save(datExpr,file = paste0(celpath,"/",fileexpr))
+  filemeta <- paste0(celpath,"/datMeta_",accession,".RData")
+  save(datMeta,file = filemeta)
+  fileexpr <- paste0(celpath,"/datExpr_",accession,".RData")
+  save(datExpr,file = fileexpr)
 }
 
 
 ExtractAffy("GSE28644","Benton_Ms_SSRI_2012")
-ExtractAffy("GSE76700","Furukawa_MsCortex_Benzo_2016")
-ExtractAffy("GSE2880","Hassel_RatFC_Anticonvulsant_201")
+ExtractAffy("GSE2880","Hassel_RatFC_Anticonvulsant_2010")
 ExtractAffy("GSE66276","Lanz_Rat_MoodStabilizer2015")
 ExtractAffy("GSE43748","Noh_RatPFC_Stimulant_2013")
-ExtractAffy("GSE93041","Orozco_Ms_Ketamine_2017")
-ExtractAffy("GSE56028","Patr├нcio_Rat_SSRI_2015")
 ExtractAffy("GSE33619","Sadasivan_MsSN_stimulant_2012")
 ExtractAffy("GSE43261","Samuels_MsHC_SSRI_2013")
 ExtractAffy("GSE28435","Spradling_Rat_Anticholinergic_2011")
 ExtractAffy("GSE56780","Zhao_MsHC_Anticonvulsant_2017")
 
+ExtractAffy("GSE76700","Furukawa_MsCortex_Benzo_2016",oligo=TRUE)
+ExtractAffy("GSE93041","Orozco_Ms_Ketamine_2017",oligo=TRUE)
+ExtractAffy("GSE56028","Patr├нcio_Rat_SSRI_2015",oligo=TRUE)
